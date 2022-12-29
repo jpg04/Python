0ebd4ad1a1184f092b98bd5d3a2d5b5d8f5e7c4c
Root Coder <jaygodase2157@gmail.com>
2022-12-30 00:04:24 +0530

    Adding Basic programs of Python

---
+++ CommentsInPy.py
@@ -0,0 +1,7 @@
+#Single Line Comment
+print("Hello Python")
+
+""" In This Code we are 
+writing Multiple line Comments """
+
+print("Hello Dear Learner")
---
+++ PyVariables.py
@@ -0,0 +1,7 @@
+a = 5 #int
+b = "Jaydeep" #string
+c = 3.5 #float
+
+print(a)
+print(b)
+print(c)
